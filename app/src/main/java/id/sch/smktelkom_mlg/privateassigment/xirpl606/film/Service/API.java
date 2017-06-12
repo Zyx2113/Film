7ba22cbe3a899d77da9e502b19586bf68d0a1a7b
arthur <arthur_untu_24rpl@student.smktelkom-mlg.sch.id>
2017-06-12 19:42:36 +0700

    done

---
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassigment/xirpl606/film/Service/API.java
@@ -0,0 +1,6 @@
+package id.sch.smktelkom_mlg.privateassigment.xirpl606.film.Service;
+
+public class API {
+    static public String API_URL = "http://api.themoviedb.org/3/discover/movie";
+    static public String API_KEY = "a867b7da110c643955a12e3c96785e6c";
+}
